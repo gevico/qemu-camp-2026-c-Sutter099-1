c1dc66d6fb68b94b699929939ee97080d824712c
Ze Huang <huang.ze@linux.dev>
2026-04-13 17:02:24 +0800

    test 09
    split by space

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef enum {
+    SPACE,
+    WORD,
+} char_type;
 
 int main() {
     char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
     int wordCount = 0;
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
+    int len = strlen(str);
+    char_type last = SPACE;
+
+    for (int i = 0; i < len; ++i) {
+        if (str[i] == ' ') {
+            if (last == WORD)
+                wordCount++;
+            last = SPACE;
+        } else {
+            last = WORD;
+        }
+    }
+
+    if (last == WORD)
+        wordCount++;
+
     printf("单词数量: %d\n", wordCount);
     
     return 0;
