abe81ce432bc9028a227507704d4bebcd7034c52
Ze Huang <huang.ze@linux.dev>
2026-04-13 17:04:38 +0800

    test 10
    memcpy

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
     
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int max_len = 101;
+    int src_len = strlen(source);
+
+    max_len = max_len < src_len ? max_len : src_len;
+    memcpy(destination, source, max_len);
     
     printf("拷贝后的字符串: %s", destination);
     
