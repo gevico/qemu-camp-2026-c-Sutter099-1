05a8cbbbde1db57a73d949e9d955d73f465d50ed
Ze Huang <huang.ze@linux.dev>
2026-04-15 16:48:37 +0800

    test 21

--- exercises/21_singly_linked_list_josephus/21_singly_linked_list_josephus.c
+++ exercises/21_singly_linked_list_josephus/21_singly_linked_list_josephus.c
@@ -62,20 +62,17 @@ void josephus_problem(int n, int k, int m) {
     }
 
     // 依次出列并打印顺序
+    link next = NULL;
+    int step;
+
     for (int out = 0; out < n; ++out) {
-        if (m == 1) {
-            // m==1 时当前节点直接出列
-            // TODO: 在这里添加你的代码
-            // I AM NOT DONE
-        }
+        for (step = 1; step < m; ++step)
+            current = next_wrap(current);
 
-        // 数到 m 的那个人出列：从 current 开始走 m-1 步，落在第 m 个节点
-        // TODO: 在这里添加你的代码
-        // I AM NOT DONE
-
-        // 此时 current 指向要出列的人
-        // TODO: 在这里添加你的代码
-        // I AM NOT DONE
+        next = next_wrap(current);
+        printf("%d ", current->item);
+        delete(current);
+        current = next;
     }
 
     printf("\n");
