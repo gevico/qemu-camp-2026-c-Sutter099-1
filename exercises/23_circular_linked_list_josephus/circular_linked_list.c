021b216d3ffa493272101e2bd82f1452e6f866ed
Ze Huang <huang.ze@linux.dev>
2026-04-15 17:49:50 +0800

    test 23

--- exercises/23_circular_linked_list_josephus/circular_linked_list.c
+++ exercises/23_circular_linked_list_josephus/circular_linked_list.c
@@ -4,11 +4,36 @@
 #include <stdlib.h>
 
 Node* create_circular_list(int n) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    Node *head, *item, *prev;
+
+    if (n <= 0)
+        return NULL;
+
+    head = malloc(sizeof(struct Node));
+    head->id = 1;
+    head->next = head;
+    prev = head;
+
+    for (int i = 2; i <= n; ++i) {
+        item = malloc(sizeof(struct Node));
+        item->id = i;
+        item->next = head;
+        prev->next = item;
+        prev = item;
+    }
+
+    return head;
 }
 
 void free_list(Node* head) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    Node *item = head, *next;
+
+    if (!head)
+        return;
+
+    while (item) {
+        next = item->next;
+        free(item);
+        item = next;
+    }
 }
