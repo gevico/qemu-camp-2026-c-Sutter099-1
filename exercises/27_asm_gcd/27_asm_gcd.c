5dea66afdfda58357c5a6d8fcf515ada66a55a34
Ze Huang <huang.ze@linux.dev>
2026-04-16 17:30:19 +0800

    test 27

--- exercises/27_asm_gcd/27_asm_gcd.c
+++ exercises/27_asm_gcd/27_asm_gcd.c
@@ -3,24 +3,26 @@
 unsigned int gcd_asm(unsigned int a, unsigned int b) {
     unsigned int result;
     
-    // TODO: 替换下方的TODO段
-    // I AM NOT DONE
     __asm__ volatile (
         "mov %1, %%eax\n\t"     // 将a存入eax
         "mov %2, %%ebx\n\t"     // 将b存入ebx
-        "jmp .L_check\n\t"      // 跳转到检查条件
         
         ".L_loop:\n\t"
-        "   TODO\n\t"
+        "   cmp %%ebx, %%eax\n\t"
+        "   je .L_done\n\t"
+        "   ja .L_sub_a\n\t"
+        "   sub %%eax, %%ebx\n\t"
+        "   jmp .L_loop\n\t"
         
-        ".L_check:\n\t"
-        "   test TODO\n\t"      // 测试ebx是否为0
-        "   jne .L_loop\n\t"    // 如果不为0，继续循环
+        ".L_sub_a:\n\t"
+        "   sub %%ebx, %%eax\n\t"
+        "   jmp .L_loop\n\t"    // 继续循环
         
+        ".L_done:\n\t"
         "mov %%eax, %0"         // 将结果（eax）存入输出变量
-        : "=r" (TODO)           // 输出操作数
-        : "r" (TODO), "r" (TODO)      // 输入操作数
-        : "TODO", "TODO", "TODO" // 被修改的寄存器
+        : "=r" (result)         // 输出操作数
+        : "r" (a), "r" (b)      // 输入操作数
+        : "eax", "ebx"          // 被修改的寄存器
     );
     
     return result;
